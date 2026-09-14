13e3017b7033ef403177581cd31c03a634461890
Natalie Rekai <ncrekai@gmail.com>
2026-09-14 19:01:44 -0400

    works but is slow

--- src/components/forms/QuestionSelect.tsx
+++ src/components/forms/QuestionSelect.tsx
@@ -1,20 +1,24 @@
-import { Button, ButtonText, FormControl, FormControlLabel, FormControlLabelText, HStack, Select, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectIcon, SelectInput, SelectItem, SelectPortal, SelectTrigger } from '@/gluestack/index';
+import { Button, ButtonText, FormControl, FormControlLabel, FormControlLabelText, HStack, Select, SelectBackdrop, SelectContent, SelectDragIndicator, SelectDragIndicatorWrapper, SelectIcon, SelectInput, SelectItem, SelectPortal, SelectTrigger, VStack } from '@/gluestack/index';
+import { Menu, MenuItem, MenuItemLabel } from '@/gluestack/index';
 import { questionBase } from '@/src/constants/questions';
 import type { QuestionBase } from '@/src/types';
 import { ChevronDownIcon } from 'lucide-react-native';
-import { useController, useForm, type DefaultValues } from 'react-hook-form';
+import { useEffect } from 'react';
+import { Controller, useController, useForm, type DefaultValues } from 'react-hook-form';
+import { Text } from 'react-native';
 
-type FormInput = { selectedType:QuestionBase | null }
 type DropDownProps = { control:any, name:string, label:string, options:QuestionBase[] }
-const formDefault:DefaultValues<FormInput> = { selectedType: null }
 
 
 const QuestionSelect = ({updateSelect}) => {
-   const { control, handleSubmit } = useForm<FormInput>({ defaultValues: formDefault });
-
+   type FormInput = { selectedType:QuestionBase | null }
+   const formDefault:DefaultValues<FormInput> = { selectedType: null }
+   const { control, handleSubmit, watch } = useForm<FormInput>({ defaultValues: formDefault });
+   const testWatch = watch()
    return (
       <HStack space='lg'>
-         <Dropdown control={control} name='selectedType' label='Select Question Type' options={questionBase} />
+         {/* <Dropdown control={control} name='selectedType' label='Select Question Type' options={questionBase} /> */}
+         <SelectionMenu control={control} name='selectedType' label='Select Question Type' options={questionBase} />
          <Button onPress={handleSubmit(updateSelect)}>
             <ButtonText>Select</ButtonText>
          </Button>
@@ -48,6 +52,40 @@ const Dropdown = ({control, name, label, options}:DropDownProps) => {
                </SelectContent>
             </SelectPortal>
          </Select>
+      </FormControl>
+   )
+}
+
+const SelectionMenu = ({control, name, label, options}:DropDownProps) => {
+   const { field: { onChange, value } } = useController({ control, name });
+   return (
+      <FormControl>
+         <FormControlLabel>
+            <FormControlLabelText>{label}</FormControlLabelText>
+         </FormControlLabel>
+         <Controller 
+            control={control}
+            name={name}
+            rules={{ required: 'Please select a question type' }}
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+               <VStack space='md'>
+                  <Menu placement='bottom left' offset={5} disabledKeys={['Settings']} closeOnSelect={true}
+                     trigger={({ ...triggerProps }) => {
+                        return (
+                           <Button {...triggerProps}>
+                              <ButtonText>{'Select'}</ButtonText>
+                           </Button>
+                        );
+                     }}>
+                     {options.map((opt) => (
+                        <MenuItem key={opt.id} textValue={opt.type} onPress={() => onChange(opt)}>
+                           <MenuItemLabel>{opt.typeLabel}</MenuItemLabel>
+                        </MenuItem> ))}
+                  </Menu>
+                  { error && <Text>{error.message}</Text> }
+               </VStack>
+            )}
+          />
       </FormControl>
    )
 }
